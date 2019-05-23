190583786502a925ae4e1eec3f2b77ad8ae329cb
Yuu Mei <nhokutevip0983@gmail.com>
2019-05-23 13:08:49 +0700

    Added watchdog

---
+++ 3rd - 2 sem/Mini IoT project/LoadCell/_Watchdog.ino
@@ -0,0 +1,14 @@
+#include <Ticker.h>
+
+Ticker sTick;
+
+volatile int wd_count = 0;
+
+void ISRWatchdog(){
+  wd_count++;
+  if (wd_count == 30){
+    Serial.println();
+    Serial.println("Watchdog bite, resetting....");
+    ESP.reset();
+  }
+}
